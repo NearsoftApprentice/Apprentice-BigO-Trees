81929471d5a1ad9dbc482dc880d0b980c75b14a2
Ramiro Jiménez <rjimenez@nearsoft.com>
2017-04-14 16:41:51 -0500

    Adding assert to the test

--- src/test/java/org/nearsoft/node/TraversalTest.java
+++ src/test/java/org/nearsoft/node/TraversalTest.java
@@ -1,5 +1,6 @@
 package org.nearsoft.node;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,10 +11,19 @@ public class TraversalTest {
 
     @Before
     public void before() {
-        Node leftNodeAA = new Node(null, null, null, 12);
-        Node rightNodeAB = new Node(null, null, null, 23);
-        Node leftNodeBA = new Node(null, null, null, 54);
+
+        Node leftAAA = new Node(null, null, null, 9);
+        Node rightAAB = new Node(null, null, null, 14);
+        Node leftABA = new Node(null, null, null, 19);
+        Node leftBAB = new Node(null, null, null, 67);
+
+
+        Node leftNodeAA = new Node(leftAAA, rightAAB, null, 12);
+        Node rightNodeAB = new Node(leftABA, null, null, 23);
+        Node leftNodeBA = new Node(null, leftBAB, null, 54);
         Node rightNodeBB = new Node(null, null, null, 76);
+
+
         Node leftNodeA = new Node(leftNodeAA, rightNodeAB, null, 17);
         Node rightNodeB = new Node(leftNodeBA, rightNodeBB, null, 72);
         root = new Node(null, null, null, 50);
@@ -23,11 +33,12 @@ public class TraversalTest {
         leftNodeA.setRightNode(rightNodeAB);
         rightNodeB.setLeftNode(leftNodeBA);
         rightNodeB.setRightNode(rightNodeBB);
+
+
     }
 
     @Test
     public void testTraversal() {
-        System.out.println(Node.inOrderTraversal(root));
-
+        Assert.assertEquals("9-12-14-17-19-23-50-54-67-72-76", Node.inOrderTraversal(root).toString());
     }
 }
